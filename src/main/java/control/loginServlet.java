7c0ecc67a415fbc3c00857e3b86e86ff9fc4c343
VoPhong25 <voquocphong6678@gmail.com>
2023-12-27 23:21:46 +0700

    login and sign out

--- src/main/java/control/loginServlet.java
+++ src/main/java/control/loginServlet.java
@@ -9,23 +9,32 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+
 
 @WebServlet(value = "/login")
 public class loginServlet extends HttpServlet {
-    DAOLogin dao= new DAOLogin();
+    public String error = "Bạn nhập sai tài khoản hoặc mật khẩu, vui lòng nhập lại!";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doPost(req,resp);
+        doPost(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    String username=req.getParameter("email");
-    String password=req.getParameter("password");
-    User user= new User(username,password);
-    boolean result=dao.checkUser(user);
-    if(result==true)
-        resp.sendRedirect(req.getContextPath() +"/index.jsp");
+        String username = req.getParameter("email");
+        String password = req.getParameter("password");
+        DAOLogin daoLogin = new DAOLogin();
+        User user = daoLogin.checkUser(username, password);
+        if (user != null) {
+            HttpSession session = req.getSession();
+            session.setAttribute("auth", user);
+            resp.sendRedirect("index.jsp");
+        } else {
+            req.setAttribute("error", error);
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
+        }
     }
 }
